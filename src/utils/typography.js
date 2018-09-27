cef8517c699ae989f5ddf450b2eabf17f95ba073
junhobaik <junhobaik@gmail.com>
2018-09-27 16:54:16 +0900

    typography 설치, 적용 실패*
    - 웹을 띄우면 순간적으로 typography가 적용된 것 같다가 곧바로 다시 적용되지 않은 모습으로 돌아옴

---
+++ src/utils/typography.js
@@ -0,0 +1,10 @@
+import Typography from 'typography';
+
+const typography = new Typography({
+  baseFontSize: '99px',
+  baseLineHeight: 1.666,
+})
+
+const { rhythm, scale } = typography
+
+export { rhythm, scale, typography as default }
